859bb03d6ff0c4ad99fe45da34b7da65ea8fcbee
Jess-unv <158201616+Jess-unv@users.noreply.github.com>
2024-08-15 17:10:20 -0700

    api

---
+++ servidor.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const puerto = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Conexión a la base de datos
+mongoose.connect('mongodb+srv://mongoomongo465:xw5jacjU8kqFOUfR@cluster0.chcllyw.mongodb.net/inventario?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Conectado a MongoDB');
+}).catch(err => {
+  console.error('Error de conexión a la base de datos', err);
+});
+
+// Rutas
+const rutasComputadoras = require('./rutas/computadoraRutas');
+const rutasUsuario = require('./rutas/usuarioRutas'); // Importa la ruta de usuarios
+
+app.use('/api/computadoras', rutasComputadoras);
+app.use('/api/usuarios', rutasUsuario); // Usa la ruta de usuarios
+
+// Iniciar el servidor
+app.listen(puerto, () => {
+  console.log(`Servidor está corriendo en http://localhost:${puerto}`);
+});
